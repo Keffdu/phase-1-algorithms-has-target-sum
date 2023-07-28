64bf0bb064e6972924feadc96560941cf2f12e7a
Kevin Dutzy <kddutz15@gmail.com>
2023-07-27 23:45:26 -0700

    commit tree cda23d065ea35df8a808330c9b6f2dc348c84db0

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  let newArr
+  for (let i = 0; i <= array.length; i++) {
+  for (let j = i + 1; j < array.length; j++) {
+    console.log(array[i], array[j], array[i] + array[j], target)
+    if (array[i] + array[j] === target) {
+      return true
+    }
+  }
+}
+  return false
 }
 
 /* 
@@ -18,12 +28,15 @@ function hasTargetSum(array, target) {
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
-  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+  console.log("=>", hasTargetSum([11, 8, 12, 4, 3, 7], 10));
 
   console.log("");
 
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
+  
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([-1, 19, 4, 6, 26], 25));
 
   console.log("");
 
